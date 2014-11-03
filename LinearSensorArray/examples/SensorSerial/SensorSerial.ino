1466e4426c4ae203e3fafaec8cda59961a5338b0
Peter Polidoro <peterpolidoro@gmail.com>
2014-11-03 14:40:29 -0500

    Added example wiring.

--- LinearSensorArray/examples/SensorSerial/SensorSerial.ino
+++ LinearSensorArray/examples/SensorSerial/SensorSerial.ino
@@ -5,17 +5,21 @@
 
 const uint8_t si_pin = 2;
 const uint8_t clk_pin = 3;
-//const uint8_t pixel_per_sensor_count = 128;
+
+// Serial Connection
 const int pixel_per_sensor_count = 768;
-//#define SENSOR_COUNT 3
-#define SENSOR_COUNT 1
-//const int analog_pins[SENSOR_COUNT] = {0,1,2};
-const uint8_t analog_pins[SENSOR_COUNT] = {0};
+const int sensor_count = 1;
+const uint8_t analog_pins[sensor_count] = {0};
+
+// Parallel Connection
+//const uint8_t pixel_per_sensor_count = 384;
+//const int sensor_count = 2
+//const int analog_pins[sensor_count] = {0,1};
 
 // pixel values: 0-1023
 
 LinearSensorArray  linear_sensor_array(si_pin, clk_pin, analog_pins);
-LinearSensorArrayImage image(SENSOR_COUNT,pixel_per_sensor_count);
+LinearSensorArrayImage image(sensor_count,pixel_per_sensor_count);
 
 
 void setup()
@@ -23,7 +27,7 @@ void setup()
   linear_sensor_array.init();
 
   // Setup serial communications
-  Serial.begin(115200);
+  Serial.begin(9600);
 
 }
 

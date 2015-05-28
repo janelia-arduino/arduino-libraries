879970d4c8d9cfd1202e14f0ab85d9491e49004e
Peter Polidoro <peterpolidoro@gmail.com>
2015-05-28 14:14:06 -0400

    Added tests to example.

--- BetterMap/examples/BetterMapTester/BetterMapTester.ino
+++ BetterMap/examples/BetterMapTester/BetterMapTester.ino
@@ -44,6 +44,48 @@ void setup()
   {
     Serial << " " << out << " " << better_out_count_array[out] << endl;
   }
+
+  int in_min_test = 0;
+  int in_max_test = 10;
+  const int out_min_test = 0;
+  const int out_max_test = 100;
+  delay(1000);
+  Serial << "map(" << in_min_test << ".." << in_max_test << ", ";
+  Serial << in_min_test << ", " << in_max_test << ", ";
+  Serial << out_min_test << ", " << out_max_test << ")" << endl;
+  for(in=in_min_test; in<=in_max_test; in++)
+  {
+    out = map(in,in_min_test,in_max_test,out_min_test,out_max_test);
+    Serial << "in: " << in << " out: " << out << endl;
+  }
+  Serial << "betterMap(" << in_min_test << ".." << in_max_test << ", ";
+  Serial << in_min_test << ", " << in_max_test << ", ";
+  Serial << out_min_test << ", " << out_max_test << ")" << endl;
+  for(in=in_min_test; in<=in_max_test; in++)
+  {
+    out = betterMap(in,in_min_test,in_max_test,out_min_test,out_max_test);
+    Serial << "in: " << in << " out: " << out << endl;
+  }
+
+  in_min_test = 10;
+  in_max_test = 0;
+  delay(1000);
+  Serial << "map(" << in_min_test << ".." << in_max_test << ", ";
+  Serial << in_min_test << ", " << in_max_test << ", ";
+  Serial << out_min_test << ", " << out_max_test << ")" << endl;
+  for(in=in_min_test; in>=in_max_test; --in)
+  {
+    out = map(in,in_min_test,in_max_test,out_min_test,out_max_test);
+    Serial << "in: " << in << " out: " << out << endl;
+  }
+  Serial << "betterMap(" << in_min_test << ".." << in_max_test << ", ";
+  Serial << in_min_test << ", " << in_max_test << ", ";
+  Serial << out_min_test << ", " << out_max_test << ")" << endl;
+  for(in=in_min_test; in>=in_max_test; --in)
+  {
+    out = betterMap(in,in_min_test,in_max_test,out_min_test,out_max_test);
+    Serial << "in: " << in << " out: " << out << endl;
+  }
 }
 
 

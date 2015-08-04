6743e645a6f93c71a7eed3970a92c6898d8b301d
Peter Polidoro <peterpolidoro@gmail.com>
2015-08-04 11:02:09 -0400

    Removed spurious code.

--- FilterSmooth/FilterSmooth.cpp
+++ FilterSmooth/FilterSmooth.cpp
@@ -6,10 +6,3 @@
 // Peter Polidoro polidorop@janelia.hhmi.org
 // ----------------------------------------------------------------------------
 #include "FilterSmooth.h"
-
-long betterMap(long x, long in_min, long in_max, long out_min, long out_max)
-{
-  long mapped_x = map(x,in_min,(in_max+1),out_min,(out_max+1));
-  return constrain(mapped_x,out_min,out_max);
-}
-
